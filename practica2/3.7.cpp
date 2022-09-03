8eb39551a2d39aa34715b1fdbb35d3170cabd935
Luis-Huachaca-HV <74331112+Luis-Huachaca-HV@users.noreply.github.com>
2022-09-02 22:03:14 -0500

    Update 3.7.cpp

--- practica2/3.7.cpp
+++ practica2/3.7.cpp
@@ -4,50 +4,14 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include "NS1.cpp"
 
 
-namespace MyNS
-{
-    int gnCount = 8; // Global counter
-    // Some small function implemented inline
-    double Addition(double a, double b) // Global function
-    {
-        return a + b;
-    }
-    // Some prototypes
-    long factorial(int n);// Global function
-    class CTest
-    {
-    public:
-        CTest();
-        void Method1();
-    };
-    // More variables, functions, methods, classes, structs go here
-};
-
-long MyNS::factorial(int n)
-{
-    if (n == 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-// Constructor for class CTest
-MyNS::CTest::CTest()
-{
-    // Initialize something
-}
-void MyNS::CTest::Method1()
-{
-    std::cout << "we are on method1";
-    // You have to write your code here
-}
-// More implementation 
 int main()
 {
    
 
-        cout << MyNS::factorial(5) << endl;
-        cout << MyNS::gnCount << endl;
+        cout << MyNS::factorial(5) << endl;//120
+        cout << MyNS::gnCount << endl;//8
 }
 
